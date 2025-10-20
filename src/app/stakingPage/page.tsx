dce6aba828a748a2828d9d90920ffe6baade5989
johtieno6@gmail.com <johtieno6@gmail.com>
2025-10-21 00:36:11 +0300

    building

--- src/app/stakingPage/page.tsx
+++ src/app/stakingPage/page.tsx
@@ -4,12 +4,15 @@ import DespositButton from "../components/depositButton"
 import FooterComponent from "../components/footer"
 import { truncateTeamName } from "../components/fixtureCard"
 
+import { useState } from "react"
+
 // redux import setup
 import { AppDispatch, RootState } from "../app_state/store"
 import { UseSelector } from "react-redux"
 import { UseDispatch } from "react-redux"
 import { useSelector } from "react-redux"
 import ProtectedRoute from "../components/protectedRoute"
+import { DiscAlbum } from "lucide-react"
 
 function Staking() {
 
@@ -21,25 +24,68 @@ function Staking() {
 
     const currentMatchData : {home: string; away: string} | null = null
 
-    const handlePlaceButtonClick = () => {
+    const [stakeInitialized, setStakeInitialized]= useState<boolean>(true)
+    const [useQrCode, setUseQrCode] = useState(true)
+
+    const handlePlaceBetButtonClick = () => {
         console.log('the place bet button has been clicked')
     }
+
+    const handleQrCodeToggleButtonClick = () => {
+        setUseQrCode(!useQrCode)
+    }
+
+    const handleHomeButtonClick= () => {
+        if (currentStakeData.homeTeam === currentStakeData.placement) {
+            return;
+        }
+        
+    }
+
     return (
         <div className = "bg-background-blue min-h-screen">
            <HeaderComponent/>
            {/* the staking main content goes here now */}
-           <div className="mt-14">
+           <div className="mt-8">
                 <h2 className = "text-4xl font-bold ml-4">Staking</h2>
-                <div className= "ml-2 mt-4  flex mr-2">
+                <div className= " mx-4 mt-4  flex mr-2 ">
                     <div className ="flex flex-col w-1/2 h-35 ">
-                        <button 
-                        className ="border border-gray-500 rounded-lg bg-lightblue-components mx-2 shadow-2xl text-xl my-2 h-1/2 ">{truncateTeamName(currentStakeData.homeTeam)}</button>
-                        <button
-                        className ="border border-gray-500 rounded-lg bg-lightblue-components mx-2 shadow-2xl text-xl mb-2 h-1/2 ">{truncateTeamName(currentStakeData.awayTeam)}</button>
+                    {/* the different buttons are rendered conditionaly based on whether they are the ones in the currentstake placement */}
+                        {(currentStakeData.placement === currentStakeData.homeTeam) ? (
+                            <button 
+                            className ="border border-black text-black bg-yellow-components rounded-lg mr-2 shadow-2xl text-xl my-2 h-1/2 ">
+                                {truncateTeamName(currentStakeData.homeTeam)}
+                            </button>
+                        ) : (
+                            <button 
+                            className ="border border-gray-500 rounded-lg bg-lightblue-components mr-2 shadow-2xl text-xl my-2 h-1/2 ">
+                                {truncateTeamName(currentStakeData.homeTeam)}
+                            </button>
+                        )}
+                        {(currentStakeData.placement === currentStakeData.awayTeam) ? (
+                            <button
+                            className ="border bg-yellow-components text-black border-gray-500 rounded-lg  mr-2 shadow-2xl text-xl mb-2 h-1/2 ">
+                                    {truncateTeamName(currentStakeData.awayTeam)}
+                                </button>
+                        ) : (
+                            <button
+                            className ="border border-gray-500 rounded-lg bg-lightblue-components mr-2 shadow-2xl text-xl mb-2 h-1/2 ">
+                                {truncateTeamName(currentStakeData.awayTeam)}
+                            </button>
+                        )}
                     </div>
                     <div className="w-1/2 ">
-                        <button 
-                        className="border border-gray-500 bg-lightblue-components h-30 rounded-lg w-40 shadow-2xl mx-2 my-2  text-xl">Draw</button>
+                       {(currentStakeData.placement === "draw") ? (
+                         <button 
+                         className="border border-gray-500 text-black bg-yellow-components h-30 rounded-lg w-40 shadow-2xl mx-2 my-2  text-xl">
+                                Draw
+                            </button>
+                       ) : (
+                         <button 
+                         className="border border-gray-500 bg-lightblue-components h-30 rounded-lg w-40 shadow-2xl mx-2 my-2  text-xl">
+                            Draw
+                         </button>
+                       )}
                     </div>
                 </div>
                 {/* staking amount entrance point */}
@@ -51,12 +97,12 @@ function Staking() {
                         className = "text-white focus:outline-none focus:ring-yellow-50 focus:border-transparent hover:font-extrabold font-bold w-38 pl-4" />
                     </div>
                     <button className = "bg-yellow-components  text-center text-black px-6 py-1 rounded-full"
-                    onClick={handlePlaceButtonClick}>place bet</button>
+                    onClick={() => {handlePlaceBetButtonClick()}}>place bet</button>
                 </div>
                 {/* deposit and place bet button */}
                 <div className="mt-2 flex ml-4 flex-col">
                     <h2 className= "text-sm">quick amounts</h2>
-                    <div className="flex gap-4 mt-2 border-r py-2 border-r-gray-100 border-l border-l-gray-100 flex-1 overflow-x-auto w-88">
+                    <div className="flex rounded-full gap-4 mt-2 border-r py-2 border-r-gray-500 border-l border-l-gray-500 flex-1 overflow-x-auto w-88">
                         {quickAmountValues.map((amount) => (
                                 <button
                                 className= "px-3 py-1 rounded-full text-white  bg-lightblue-components">{amount}</button>
@@ -64,21 +110,39 @@ function Staking() {
                     </div>
                 </div>
                 {/* bet invite part */}
-                <div className = 'mt-4 ml-2'>
-                    <h2 className = "ml-4">Invite : Scan QR code below</h2>
-                    <div className = "w-65 mt-4">
-                        <img src="/example_qr.png" alt="" />
-                    </div>
-                    <div>
-                        <h2 className ="mt-2 ml-4">or copy the link below</h2>
-                        <div className = "flex gap-2 pt-2">
-                            <div className = "border-2 border-gray-600 w-50 rounded-lg px-2 py-1">
-                                aaflafjkl
+                {stakeInitialized ? (
+                     <div className = 'mt-2 ml-4 rounded-lg bg-lightblue-components flex flex-col justify-center items-center w-90 h-90'>
+                        {useQrCode ? (
+                            <div className="flex items-center justify-center flex-col">
+                                <h2 className = "ml-4">Invite : Scan QR code below</h2>
+                                <div className = " mt-2  px-2 w-70 items-center justify-center">
+                                    <img src="/example_qr.png" alt="" />
+                                </div>
+                                <button
+                                onClick={() => [handleQrCodeToggleButtonClick()]}
+                                 className= "mt-2 underline text-sm">use invite code</button>
                             </div>
-                            <button className="bg-gray-600 rounded-lg px-2 py-1 shadow-sm">copy</button>
-                        </div>
+                        ) : (
+                            <div className="mt-2 ml-4 mr-4  pb-2 shadow-2xl flex-col  rounded-lg flex items-center w-90 justify-center">
+                                 <div>
+                                    <h2>copy invite link below</h2>
+                                    <div className="flex gap-2">
+                                        <h2 className="text-xl">alfaijfhas</h2>
+                                        <button className="bg-background-blue rounded-lg px-3 py text-white">copy</button>
+                                    </div>
+
+                                 </div>
+                                 <button 
+                                 onClick={() => {handleQrCodeToggleButtonClick()}}
+                                 className='underline text-white text-sm mt-4'>use QR code</button>
+                            </div>
+                        )}
                     </div>
-                </div>
+                ) : (
+                    <div className = 'h-90 mt-2 ml-4 bg-lightblue-components pb-2 shadow-2xl mr-10 rounded-lg flex items-center justify-center'>
+                        <h2 className= "text-white text-sm">Staking...</h2>
+                    </div>
+                )}
                 {/* the footer at the bottom */}
                 <div className="mb-0 bottom-0 fixed w-full">
                     <FooterComponent/>
